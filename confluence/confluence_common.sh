f1935775bd994beca8ae9ec43cdc6a136f7a61ba
Antti Jaakkola <antti.jaakkola@remod.fi>
2016-04-06 17:16:45 +0300

    Confluence updater

---
+++ confluence/confluence_common.sh
@@ -0,0 +1,94 @@
+THIS=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+# Include helpers
+
+. ${THIS}/../helpers.sh
+
+. $CONFIG_FILE
+
+# Handle user defined parameters
+
+test_variable CONFLUENCE_BASE
+test_variable CONFLUENCE_USER
+
+test ! -z "$CONFLUENCE_SERVICE_NAME" || export CONFLUENCE_SERVICE_NAME="confluence"
+
+test -d "${CONFLUENCE_BASE}" || fail "CONFLUENCE_BASE is not a directory"
+
+export CONFLUENCE_BASE="$(realpath ${CONFLUENCE_BASE})"
+
+export BACKUPDIR="${BACKUPDIR:-${CONFLUENCE_BASE}/backup}"
+
+test ! -z "$BACKUPDIR" || fail "BACKUPDIR cannot be empty string"
+
+export DEBUG="${DEBUG:-0}"
+
+test -d "$CONFLUENCE_BASE" || fail "${CONFLUENCE_BASE} is not a directory"
+
+# Fetch some variables from confluence configurations
+
+export CONFLUENCE_CURRENT="${CONFLUENCE_BASE}/current"
+
+test -h ${CONFLUENCE_CURRENT} || fail "${CONFLUENCE_CURRENT} is not link"
+
+# Previous confluence directory
+readlink "${CONFLUENCE_CURRENT}" > /dev/null 2>&1 || fail "${CONFLUENCE_CURRENT} is a broken link"
+export CONFLUENCE_PREVIOUS="$(readlink -f ${CONFLUENCE_CURRENT})"
+
+# Get confluence version
+export CONFLUENCE_VERSION="$(get_init_value ${CONFLUENCE_PREVIOUS}/confluence/META-INF/maven/com.atlassian.confluence/confluence-webapp/pom.properties 'version')"
+
+test -z "$CONFLUENCE_VERSION" && fail "Failed to fetch confluence version from ${CONFLUENCE_PREVIOUS}/confluence/META-INF/maven/com.atlassian.confluence/confluence-webapp/pom.properties"
+
+export APPLICATIONDATABACKUPDIR="${BACKUPDIR}/application-data"
+
+export CONFLUENCE_PROPERTIES_FILE="confluence/WEB-INF/classes/confluence-init.properties"
+
+export APPLICATION_DATA_DIR="$(get_init_value ${CONFLUENCE_PREVIOUS}/${CONFLUENCE_PROPERTIES_FILE} confluence.home)"
+
+test ! -z "$APPLICATION_DATA_DIR" || fail "Failed to find application-data directory"
+
+# Get database variables, bash </3 XML
+export CONFLUENCE_DATABASE_USERNAME="$(cat ${APPLICATION_DATA_DIR}/confluence.cfg.xml | sed -ne 's/.*<property name="hibernate.connection.username">\(.*\)<\/property>.*/\1/p')"
+export CONFLUENCE_DATABASE_PASSWORD="$(cat ${APPLICATION_DATA_DIR}/confluence.cfg.xml | sed -ne 's/.*<property name="hibernate.connection.password">\(.*\)<\/property>.*/\1/p')"
+export CONFLUENCE_DATABASE_URI="$(cat ${APPLICATION_DATA_DIR}/confluence.cfg.xml | sed -ne 's/.*<property name="hibernate.connection.url">\(.*\)<\/property>.*/\1/p')"
+export CONFLUENCE_DATABASE_SERVER="$(echo $CONFLUENCE_DATABASE_URI |sed  -ne 's/.*:\/\/\(.*\):.*/\1/p')"
+export CONFLUENCE_DATABASE_PORT="$(echo $CONFLUENCE_DATABASE_URI |sed  -ne 's/.*:\/\/.*:\(.*\)\/.*/\1/p')"
+export CONFLUENCE_DATABASE_NAME="$(echo $CONFLUENCE_DATABASE_URI |sed  -ne 's/.*:\/\/.*:.*\/\(.*\)$/\1/p')"
+
+test ! -z "$CONFLUENCE_DATABASE_USERNAME" || fail "Failed to get database username for Confluence from ${APPLICATION_DATA_DIR}/confluence.cfg.xml"
+test ! -z "$CONFLUENCE_DATABASE_PASSWORD" || fail "Failed to get database password for Confluence from ${APPLICATION_DATA_DIR}/confluence.cfg.xml"
+test ! -z "$CONFLUENCE_DATABASE_SERVER" || fail "Failed to get database server address for Confluence from ${APPLICATION_DATA_DIR}/confluence.cfg.xml"
+test ! -z "$CONFLUENCE_DATABASE_PORT" || fail "Failed to get database server port for Confluence from ${APPLICATION_DATA_DIR}/confluence.cfg.xml"
+test ! -z "$CONFLUENCE_DATABASE_NAME" || fail "Failed to get database name for Confluence from ${APPLICATION_DATA_DIR}/confluence.cfg.xml"
+
+
+# Backup
+function backup_database() {
+    filename=$1
+    test -z "$filename" && fail "Dump filename cannot be empty"
+    info "Backupping database ${CONFLUENCE_DATABASE_NAME} to ${BACKUPDIR}/${filename}.gz"
+    local BACKUP_FILE="${BACKUPDIR}/${filename}.gz"
+    touch "${BACKUP_FILE}"
+    chmod 600 "${BACKUP_FILE}"
+    local PGPASSWORD="$CONFLUENCE_DATABASE_PASSWORD"
+    local PGPORT="$CONFLUENCE_DATABASE_PORT"
+    local PGHOST="$CONFLUENCE_DATABASE_SERVER"
+    local PGUSER="$CONFLUENCE_DATABASE_USERNAME"
+    pg_dump $CONFLUENCE_DATABASE_NAME |gzip -c > "${BACKUP_FILE}"
+    chmod 600 "${BACKUP_FILE}"
+}
+
+
+function backup_confluence() {
+    test -f "${CONFLUENCE_PREVIOUS}/${CONFLUENCE_PROPERTIES_FILE}" || fail "${CONFLUENCE_PREVIOUS}/${CONFLUENCE_PROPERTIES_FILE} not such file"
+
+    test -d "${BACKUPDIR}" || ( mkdir "${BACKUPDIR}"; chmod 700 "${BACKUPDIR}" )
+
+    backup_database "confluence-${CONFLUENCE_VERSION}.sql"
+
+    test -d "${APPLICATIONDATABACKUPDIR}" || ( mkdir "${APPLICATIONDATABACKUPDIR}"; chmod 700 "${APPLICATIONDATABACKUPDIR}" )
+
+    backup_files "${APPLICATION_DATA_DIR}" "${APPLICATIONDATABACKUPDIR}"
+
+}
---
+++ confluence/update_confluence.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+#
+# Update Confluence to most recent version.
+#
+
+if [ -z "$1" ]
+then
+    echo "Usage $0 path/to/config.sh"
+    exit 1
+fi
+
+export CONFIG_FILE="$1"
+
+set -e
+
+set -x
+if [ "$DEBUG" = "1" ]
+then
+    # set -x when debug
+    set -x
+fi
+
+export THIS=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+# Include commons
+. ${THIS}/confluence_common.sh
+
+# Include helpers
+. ${THIS}/../helpers.sh
+
+CONFLUENCE_TGZ="$(mktemp -u --suffix=.tar.gz)"
+
+function post_cleanup() {
+    rm $CONFLUENCE_TGZ || true
+}
+
+trap post_cleanup SIGINT SIGTERM
+
+# Get newest version
+
+CONFLUENCE_NEW_VERSION="$(latest_version confluence)"
+
+set +e
+
+vercomp "$CONFLUENCE_VERSION" "$CONFLUENCE_NEW_VERSION" '<='
+RES=$?
+set -e
+
+if [ $RES -lt 2 ]
+then
+    info "Current Confluence versio $CONFLUENCE_VERSION is up-to-date"
+    exit 0 
+fi
+
+CONFLUENCE_NEW="${CONFLUENCE_BASE}/confluence-${CONFLUENCE_NEW_VERSION}"
+
+CONFLUENCE_DOWNLOAD_URL="$(latest_version_url confluence)"
+
+info "Downloading new Confluence"
+
+wget -O "$CONFLUENCE_TGZ" "$CONFLUENCE_DOWNLOAD_URL"
+
+# Do initial backup
+
+backup_confluence
+
+# Stop Confluence
+
+if [ "${CONFLUENCE_SERVICE_NAME}" != "disable" ]
+then
+    servicemanager "${CONFLUENCE_SERVICE_NAME}" stop
+
+    # wait for Confluence to stop
+    sleep 60
+fi
+
+
+# Backup Confluence again
+
+backup_confluence
+
+#Unzip new Confluence
+
+mkdir "$CONFLUENCE_NEW"
+
+info "Unzipping new CONFLUENCE"
+tar --strip-components=1 -xf "$CONFLUENCE_TGZ" -C "$CONFLUENCE_NEW"
+
+# Remove tempdir
+rm "$CONFLUENCE_TGZ"
+
+# Restore some files from previous version
+
+info "Restoring some config files"
+
+restore_file "${CONFLUENCE_PROPERTIES_FILE}" "${CONFLUENCE_PREVIOUS}" "${CONFLUENCE_NEW}"
+
+restore_file bin/setenv.sh "${CONFLUENCE_PREVIOUS}" "${CONFLUENCE_NEW}"
+
+restore_file bin/user.sh "${CONFLUENCE_PREVIOUS}" "${CONFLUENCE_NEW}"
+
+restore_file conf/server.xml "${CONFLUENCE_PREVIOUS}" "${CONFLUENCE_NEW}"
+
+info "Setting permissions..."
+
+chown -R "$CONFLUENCE_USER" "${CONFLUENCE_NEW}/temp"
+chown -R "$CONFLUENCE_USER" "${CONFLUENCE_NEW}/logs"
+chown -R "$CONFLUENCE_USER" "${CONFLUENCE_NEW}/work"
+
+# TODO: version specific stuff here!!
+
+info "Updating current symlink"
+rm ${CONFLUENCE_CURRENT}
+ln -s ${CONFLUENCE_NEW} ${CONFLUENCE_CURRENT}
+
+info "Confluence is now updated!"
+
+if [ "${CONFLUENCE_SERVICE_NAME}" != "disable" ]
+then
+    info "Starting Confluence"
+    servicemanager "${CONFLUENCE_SERVICE_NAME}" start
+    info "Be patient, Confluence is starting up"
+fi
